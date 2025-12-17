a72b06403cd1b24b0d973059fd5ccd8ca6379bb9
nagalakshmichalla <nagalakshmichalla347@gmail.com>
2025-12-17 22:44:24 +0530

    init commit

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
+//
 public class UserController {
 
     private UserService userService;
